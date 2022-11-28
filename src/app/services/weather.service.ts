2b8501db44caf629c2f635aa3709ec1b7f96cdae
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-11-28 22:02:45 +0530

    added and done this card

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,14 +1,23 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { WeatherData } from '../models/weather.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName: string){
-    this.http.get();
+  getWeatherData(cityName: string): Observable<WeatherData>{
+    return this.http.get<WeatherData>(`${environment.weatherApiBaseUrl}${cityName}`, {
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue)
+    });
   }
 }
