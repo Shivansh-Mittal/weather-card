a73177e5f283931045bb8bfabb60993448fe934d
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-11-29 06:39:21 +0530

    finished weather card with city input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { WeatherData } from './models/weather.model';
 
+import { WeatherData } from './models/weather.model';
 import { WeatherService } from './services/weather.service';
 
 @Component({
@@ -8,21 +8,34 @@ import { WeatherService } from './services/weather.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
 
   constructor(private weatherService: WeatherService) {
 
   }
 
+  cityName: string = 'Agra';
+
   weatherData?: WeatherData;
 
   ngOnInit(): void {
-      this.weatherService.getWeatherData('Agra')
-        .subscribe({
-          next: (response) => {
-            this.weatherData = response;
-            console.log(response);
-          }
-        });
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response);
+        }
+      });
   }
 }
