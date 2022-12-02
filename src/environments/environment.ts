e180ea0a37a8762f8f7d6e20c8e157198d2ec045
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-12-02 18:56:46 +0530

    api key added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
   XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: ''
+  XRapidAPIKeyHeaderValue: 'd30f130806msh1be34de86387ed5p19a3bdjsn01a42731c955'
+  // XRapidAPIKeyHeaderValue: '51318132camshdc78e920762202cp1b6562jsne4c217c18fd7'
 };
 
 /*
