18e5cf42a4a4456663473060dfd706232d2993ba
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-12-02 19:06:23 +0530

    prod key added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'd30f130806msh1be34de86387ed5p19a3bdjsn01a42731c955'
+  // XRapidAPIKeyHeaderValue: '51318132camshdc78e920762202cp1b6562jsne4c217c18fd7'
 };
